fb47a0c465e912b88acb034bf00fc80821e97b32
Greg Fitzpatrick <gfitzpatrck2@gmail.com>
2020-01-07 11:16:22 -0500

    single shirt page styled

--- resources/views/single-shirt.blade.php
+++ resources/views/single-shirt.blade.php
@@ -1,26 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-    <div class="col-6">
-        <img class="thumbnail img-fluid" src="{{ URL::to('/') }}/images/{{ $shirt->image }}">
-    </div>
-    
-    <div class="col-6">
-        <h1>{{ $shirt->name }}</h1>
-
-        <ul>
-            <li>Gender: {{ $shirt->gender }}</li>
-            <li>Color: {{ $shirt->color }}</li>
-            <li>Size: {{ $shirt->size }}</li>
-            <li>Price: ${{ $shirt->price }}</li>
-        </ul>
+    <div class="row">
+        <div class="col-12 col-lg-6">
+            <img class="main-image d-block mx-auto img-fluid" src="{{ URL::to('/') }}/images/{{ $shirt->image }}">
+        </div>
         
-        <a href="#" class="cart-btn btn btn-primary d-flex align-items-center justify-content-center">
-            <i class="material-icons">shopping_cart</i>
-            Add to Cart
-        </a>
-    </div>
+        <div class="col-12 col-lg-6">
+            <div class="card bg-light my-3">
+                <h1 class="card-header h3">{{ $shirt->name }}</h1>
+  
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Gender: {{ $shirt->gender }}</li>
+                        <li class="list-group-item">Color: {{ $shirt->color }}</li>
+                        <li class="list-group-item">Size: {{ $shirt->size }}</li>
+                        <li class="list-group-item">Price: ${{ $shirt->price }}</li>
+                    </ul>
+                    
+                    <a href="#" class="cart-btn m-3 btn btn-primary d-flex align-items-center justify-content-center">
+                        <i class="material-icons">shopping_cart</i>
+                        Add to Cart
+                    </a>
+                </div>
+            </div>
+        </div>
 
-</div>
+    </div>
 @endsection
--- storage/framework/views/f0c419107752aab13131d95b90a62909ffe32b4d.php
+++ storage/framework/views/f0c419107752aab13131d95b90a62909ffe32b4d.php
@@ -1,25 +1,29 @@
 <?php $__env->startSection('content'); ?>
-<div class="row">
-    <div class="col-6">
-        <img class="thumbnail img-fluid" src="<?php echo e(URL::to('/')); ?>/images/<?php echo e($shirt->image); ?>">
-    </div>
-    
-    <div class="col-6">
-        <h1><?php echo e($shirt->name); ?></h1>
-
-        <ul>
-            <li>Gender: <?php echo e($shirt->gender); ?></li>
-            <li>Color: <?php echo e($shirt->color); ?></li>
-            <li>Size: <?php echo e($shirt->size); ?></li>
-            <li>Price: $<?php echo e($shirt->price); ?></li>
-        </ul>
+    <div class="row">
+        <div class="col-12 col-lg-6">
+            <img class="main-image d-block mx-auto img-fluid" src="<?php echo e(URL::to('/')); ?>/images/<?php echo e($shirt->image); ?>">
+        </div>
         
-        <a href="#" class="cart-btn btn btn-primary d-flex align-items-center justify-content-center">
-            <i class="material-icons">shopping_cart</i>
-            Add to Cart
-        </a>
-    </div>
+        <div class="col-12 col-lg-6">
+            <div class="card bg-light my-3">
+                <h1 class="card-header h3"><?php echo e($shirt->name); ?></h1>
+  
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Gender: <?php echo e($shirt->gender); ?></li>
+                        <li class="list-group-item">Color: <?php echo e($shirt->color); ?></li>
+                        <li class="list-group-item">Size: <?php echo e($shirt->size); ?></li>
+                        <li class="list-group-item">Price: $<?php echo e($shirt->price); ?></li>
+                    </ul>
+                    
+                    <a href="#" class="cart-btn m-3 btn btn-primary d-flex align-items-center justify-content-center">
+                        <i class="material-icons">shopping_cart</i>
+                        Add to Cart
+                    </a>
+                </div>
+            </div>
+        </div>
 
-</div>
+    </div>
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\gregf\Desktop\custom-tees-greg\resources\views/single-shirt.blade.php ENDPATH**/ ?>
